18d4b185137adbe2bb282705e0e8276b02dc80aa
Nizami Islamovs <nizamiislamovs@gmail.com>
2021-11-26 05:43:38 +0200

    Few adjustments for DSC + Napco related info.

--- Napco/Magnum_1008e/magnum1008e/magnum1008e.ino
+++ Napco/Magnum_1008e/magnum1008e/magnum1008e.ino
@@ -1,8 +1,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////
-//      DSC Classic alarm systems keybus monitor
-//      tested on models : DSC PC-585
-//                            
+//      Napco Magnum 1008e alarm systems keybus monitor
+//      tested on models : Magnum 1008e
 ///////////////////////////////////////////////////////////////////////////////////
 
 #define BUS_CLK             5
@@ -30,12 +29,13 @@ void loop() {
 void loopback() {
 
     while(1) {
-        waitForPause(500);        
+//      for(int n=0; n<=3; n++) {    //for better noise tolerance
+        waitForPause();
         waitForPackageStart();
-//        displayClockCount();
+  //      displayClockCount();
         packet = readPacket();
-//        delayMicroseconds(50);
-
+        delayMicroseconds(50);
+//      }
       String pkt = "";
       for (int n=0; n<packet.length(); n++) {
         if (n%8 == 0) {
@@ -46,17 +46,17 @@ void loopback() {
 
       Serial.print(pkt + " ");
       if(!packet.equals(previousPacket)) {
-//        processPacket();
+        processPacket();
         previousPacket = packet;
       }
       Serial.println();
-      delayMicroseconds(3550);
+      delayMicroseconds(150);
     }
 }
 
 void processPacket() {
   int bitNum = 0;
-   while (bitNum < 83) {
+   while (bitNum < 47) {
     if(packet.charAt(bitNum) != previousPacket.charAt(bitNum) && packet.charAt(bitNum) == '0') {
       switch(bitNum) {
         case 0: Serial.print("TRBL pressed"); break;
@@ -94,33 +94,33 @@ String readPacket() {
   String packet ="";
   while(1) {
     int val = digitalRead(BUS_CLK);
-    if(val == LOW) {
+    if(val == HIGH) {
 
       
-      delayMicroseconds(150);
+      delayMicroseconds(100);
       
       val = digitalRead(BUS_CLK);
-        if(val == LOW) {
+        if(val == HIGH) {
                 totalDelayUs=0;
       clkCount++;
         
       dataBit = digitalRead(BUS_DATA);
       
-      if(previousPacket.length() < 84)
+      if(previousPacket.length() < 48)
         previousPacket.concat(String(dataBit));
 
       packet.concat(String(dataBit));
       
       while(1) {
         val = digitalRead(BUS_CLK);
-        if(val == HIGH) {
+        if(val == LOW) {
           //button pushed
-          delayMicroseconds(150);
+          delayMicroseconds(100);
           val = digitalRead(BUS_CLK);
-          if(val == HIGH) {
+          if(val == LOW) {
             dataBit = digitalRead(BUS_DATA);
   
-            if(previousPacket.length() < 84)
+            if(previousPacket.length() < 48)
               previousPacket.concat(String(dataBit));
             
             packet.concat(String(dataBit));
@@ -134,7 +134,7 @@ String readPacket() {
     }
     delayMicroseconds(25);
     totalDelayUs+=25;
-    if (totalDelayUs >= 1000) {
+    if (totalDelayUs >= 1250) {
       totalDelayUs = 0;
       return packet;
 //      return clkCount;
@@ -142,20 +142,18 @@ String readPacket() {
   }
 }
 
-
-
-void waitForPause(int dly) {
+void waitForPause() {
   while(1) {
     int val = digitalRead(BUS_CLK);
     
-    if(val == LOW) {
-      delayMicroseconds(100);
+    if(val == HIGH) {
+      delayMicroseconds(150);
       totalDelayUs = 0;
     } else {
       
-      delayMicroseconds(25);
-      totalDelayUs+=25;
-      if (totalDelayUs >= dly) {
+      delayMicroseconds(50);
+      totalDelayUs+=50;
+      if (totalDelayUs >= 2000) {
         totalDelayUs = 0;
         return;
       }
@@ -166,7 +164,7 @@ void waitForPause(int dly) {
 void waitForPackageStart() {
   while(1) {
     int val = digitalRead(BUS_CLK);
-    if(val == LOW) {
+    if(val == HIGH) {
       return;
     } 
     delayMicroseconds(2);
@@ -188,7 +186,7 @@ int displayClockCount() {
     } 
     delayMicroseconds(50);
     totalDelayUs+=50;
-    if (totalDelayUs >= 3500) {
+    if (totalDelayUs >= 1000) {
       totalDelayUs = 0;
       return clkCount;
     }
